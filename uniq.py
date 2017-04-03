89979fbb46ab1aab6e61f62a0bf3ea12bf8e2d62
aib <aibok42@gmail.com>
2017-04-03 18:36:16 +0300

    Remove unnecessary files

--- uniq.py
+++
@@ -1,9 +0,0 @@
-seen = set()
-with open('asksozleri.txt', 'r') as f:
-	with open('asksozleri_u.txt', 'w') as fo:
-		for line in f:
-			if line != "\n":
-				if line in seen:
-					continue
-			fo.write(line)
-			seen.add(line)
